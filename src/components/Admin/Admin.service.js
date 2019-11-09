f35653ac3075d0dcd1ab775e74ed74a4acef1cf1
Benjamin Lacroix <blacroix@xebia.fr>
2019-11-09 15:08:39 +0100

    feat(admin): admin dashboard

---
+++ src/components/Admin/Admin.service.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const voteUniverse = async (universe) =>
+  await axios.post(`${process.env.REACT_APP_BACKEND_HOST}/api/universe`, universe);
+
+export const stopPoll = async () =>
+  await axios.post(`${process.env.REACT_APP_BACKEND_HOST}/api/poll/stop`);
+
+export const resetPoll = async () =>
+  await axios.post(`${process.env.REACT_APP_BACKEND_HOST}/api/poll/reset`);
