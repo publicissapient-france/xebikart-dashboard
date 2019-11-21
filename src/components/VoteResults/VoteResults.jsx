b4df76f444c18045831addbce29085f73c2582ad
tsimonnet <tsimonnet@xebia.fr>
2019-11-21 10:19:16 +0100

    fix: tweaks on vote page

--- src/components/VoteResults/VoteResults.jsx
+++ src/components/VoteResults/VoteResults.jsx
@@ -12,108 +12,127 @@ import cadreImage from "./cadre.svg";
 
 export default ({ className }) => {
   console.log("render");
-  const state = useSSE("SurveyVoteReceived", {
+  const votes = useSSE("SurveyVoteReceived", {
     initialState: {
-      zombieVotes: 0,
-      unicornVotes: 0
+      zombie: 0,
+      unicorn: 0
     },
     stateReducer: (state, changes) => {
       if (changes.data.vote.choice === "1") {
         return {
           ...state,
-          zombieVotes: state.zombieVotes + 1
+          zombie: state.zombie + 1
         };
       } else if (changes.data.vote.choice === "2") {
         return {
           ...state,
-          unicornVotes: state.unicornVotes + 1
+          unicorn: state.unicorn + 1
         };
+      }
+    }
+  });
+
+  const winner = useSSE("SurveyCompleted", {
+    initialState: undefined,
+    stateReducer: (state, changes) => {
+      if (changes.data.votes["1"] > changes.data.votes["2"]) {
+        return "zombie";
+      } else if (changes.data.votes["2"] > changes.data.votes["1"]) {
+        return "unicorn";
+      } else {
+        return "draw";
       }
     }
   });
 
   return (
     <div className={classnames(styles.container, className)}>
-      <div className={styles.container__left}>
-        <div className={styles.container__character}>
-          <img
-            alt="zombie-frame"
-            src={cadreImage}
-            className={styles.container__character__frame}
-          />
-          <img
-            alt="zombie"
-            src={zombieImage}
-            className={styles.container__left__zombie}
-          />
-        </div>
-        <img
-          alt="zombie-text"
-          src={zombieText}
-          className={styles.container__characterName}
-        />
-        <div className={styles.container__digits}>
-          000
-          <div className={styles.container__digits__full}>
-            {state.zombieVotes}
+      {winner ? (
+        <div>{winner}</div>
+      ) : (
+        <>
+          <div className={styles.container__left}>
+            <div className={styles.container__character}>
+              <img
+                alt="zombie-frame"
+                src={cadreImage}
+                className={styles.container__character__frame}
+              />
+              <img
+                alt="zombie"
+                src={zombieImage}
+                className={styles.container__left__zombie}
+              />
+            </div>
+            <img
+              alt="zombie-text"
+              src={zombieText}
+              className={styles.container__characterName}
+            />
+            <div className={styles.container__digits}>
+              000
+              <div className={styles.container__digits__full}>
+                {votes.zombie}
+              </div>
+            </div>
+            <div className={styles.container__emptyDigits}>000</div>
           </div>
-        </div>
-        <div className={styles.container__emptyDigits}>000</div>
-      </div>
-      <div className={styles.container__right}>
-        <div className={styles.container__character}>
-          <img
-            alt="unicorn-frame"
-            src={cadreImage}
-            className={styles.container__character__frame}
-          />
-          <img
-            alt="unicorn"
-            src={unicornImage}
-            className={styles.container__right__unicorn}
-          />
-        </div>
-        <img
-          alt="unicorn-text"
-          src={unicornText}
-          className={styles.container__characterName}
-        />
-        <div className={styles.container__digits}>
-          000
-          <div className={styles.container__digits__full}>
-            {state.unicornVotes}
+          <div className={styles.container__right}>
+            <div className={styles.container__character}>
+              <img
+                alt="unicorn-frame"
+                src={cadreImage}
+                className={styles.container__character__frame}
+              />
+              <img
+                alt="unicorn"
+                src={unicornImage}
+                className={styles.container__right__unicorn}
+              />
+            </div>
+            <img
+              alt="unicorn-text"
+              src={unicornText}
+              className={styles.container__characterName}
+            />
+            <div className={styles.container__digits}>
+              000
+              <div className={styles.container__digits__full}>
+                {votes.unicorn}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className={styles.container__bottom}>
-        <div
-          className={classnames(
-            styles.container__bottom__progress,
-            styles["container__bottom__progress--left"]
-          )}
-          style={{ width: `${state.zombieVotes / 10}%` }}
-        />
-        <div
-          className={classnames(
-            styles.container__bottom__progress,
-            styles["container__bottom__progress--right"]
-          )}
-          style={{ width: `${state.unicornVotes / 10}%` }}
-        />
-        {Array(20)
-          .fill(0)
-          .map((_, index) => (
-            <div className={styles.container__bottom__emptyBar}></div>
-          ))}
-        <div
-          style={{ left: `calc(${state.zombieVotes / 10}% - 0.75vh)` }}
-          className={styles.container__bottom__progressPoint}
-        />
-        <div
-          style={{ right: `calc(${state.unicornVotes / 10}% - 0.75vh)` }}
-          className={styles.container__bottom__progressPoint}
-        />
-      </div>
+          <div className={styles.container__bottom}>
+            <div
+              className={classnames(
+                styles.container__bottom__progress,
+                styles["container__bottom__progress--left"]
+              )}
+              style={{ width: `${votes.zombie / 10}%` }}
+            />
+            <div
+              className={classnames(
+                styles.container__bottom__progress,
+                styles["container__bottom__progress--right"]
+              )}
+              style={{ width: `${votes.unicorn / 10}%` }}
+            />
+            {Array(20)
+              .fill(0)
+              .map((_, index) => (
+                <div className={styles.container__bottom__emptyBar}></div>
+              ))}
+            <div
+              style={{ left: `calc(${votes.zombie / 10}% - 0.75vh)` }}
+              className={styles.container__bottom__progressPoint}
+            />
+            <div
+              style={{ right: `calc(${votes.unicorn / 10}% - 0.75vh)` }}
+              className={styles.container__bottom__progressPoint}
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 };
